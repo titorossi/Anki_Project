9d43cbec5cda5a010c6bb07ec5ab4ce8560801a9
titorossi <105487821+titorossi@users.noreply.github.com>
2023-10-15 11:26:52 +0200

    Add files via upload

---
+++ anki_paste.py
@@ -0,0 +1,30 @@
+import requests
+import json
+
+def invoke(action, params={}):
+    request_json = json.dumps({'action': action, 'params': params, 'version': 6})
+    response = requests.post('http://localhost:8765', data=request_json)
+    return response.json()
+
+def add_note(deck_name, fields):
+    note = {
+        "deckName": deck_name,
+        "modelName": "Artillery",
+        "fields": fields,
+        "options": {
+            "allowDuplicate": False
+        },
+        "tags": []
+    }
+    invoke('addNote', {'note': note})
+
+# Example usage:
+fields = {
+    "Native word": "ciao",
+    "Native phrase": "Ciao, come stai?",
+    "Definition": "A common Italian greeting.",
+    "Foreign word": "hello",
+    "Foreign phrase": "Hello, how are you?",
+    "Foreign phrase audio": "[sound:example_audio.mp3]"
+}
+add_note("test", fields)
---
+++ deepl_trans (incomplete).py
@@ -0,0 +1,1 @@
+# this is for the deepl translation component
