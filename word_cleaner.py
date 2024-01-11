1d6166895360a26ddf90fc6f1f416537df0ca70c
titorossi <titotr22@gmail.com>
2024-01-11 14:34:15 +0100

    Streamlined code

--- word_cleaner.py
+++ word_cleaner.py
@@ -1,37 +1,21 @@
-
 input_file = input('Please define an input file: ')
 output_file = input('Please define an output file: ')
 
-# Open the input file and add all lines that do not start with a digit to the output file
+# Open the input file and process lines as required, then write to the output file
 with open(input_file, 'r') as fin, open(output_file, 'w') as fout:
     for line in fin:
-        if line[0].isdigit():
-            fout.write(line)
+        
+        # Skip lines starting with a digit
+        if not line[0].isdigit():
+            continue
 
-# Reopen the output file and remove all numbers from it
-with open(output_file, 'r') as fin:
-    lines = fin.readlines()
+        # Remove all digits from the line
+        line = ''.join(char for char in line if not char.isdigit())
 
-with open(output_file, 'w') as fout:
-    for line in lines:
-        fout.write(''.join(char for char in line if not char.isdigit()))
+        # Skip lines that are empty or contain only semicolons after removing digits
+        if not (line.strip() and not all(char in ';\n\r' for char in line)):
+            continue
 
-#Reopen the output file and remove all empty lines from it
-with open(output_file, 'r') as fin:
-    lines = fin.readlines()
-
-with open(output_file, 'w') as fout:
-    for line in lines:
-        if line.strip() and not all(char in ';\n\r' for char in line):
-            fout.write(line)
-
-# Reopen the output file and remove all words after the first word from it
-with open(output_file, 'r') as fin:
-    lines = fin.readlines()
-
-with open(output_file, 'w') as fout:
-    for line in lines:
-        words = line.split()
-        if words:
-            fout.write(words[0] + '\n')
-
+        # Extract the first word and write it to the output file
+        first_word = line.split()[0]
+        fout.write(first_word + '\n')
